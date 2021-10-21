9189b46c10446acec0bd7a6d9a0a685aa43ae19c
zalecki-synopsys <zalecki@synopsys.com>
2021-10-22 00:36:11 +0100

    adding fiz for default recurence

---
+++ migrations/20211021233442_create_set_none_recurences.postgres.down.sql
@@ -0,0 +1,1 @@
+DELETE FROM transactions_recurence WHERE ID=1;
---
+++ migrations/20211021233442_create_set_none_recurences.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO "public"."transactions_recurence"("id","name","description","addtime","created_at","updated_at")
+VALUES
+(1,E'none',E'none',E'0-0-0',E'2021-10-20 22:36:20.219156',E'2021-10-20 22:36:20.219156');
